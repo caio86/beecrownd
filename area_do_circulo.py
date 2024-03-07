af68c288967a5808cc5ea753bd44152c6f5e1cbb
caio86 <caioluiz86@gmail.com>
2024-03-07 10:20:48 -0300

    atividades dia: 07/03/2024

---
+++ area_do_circulo.py
@@ -0,0 +1,7 @@
+raio = float(input("Digite o raio de um círculo: "))
+
+PI = 3.14159
+
+area = PI * (raio ** 2)
+
+print(f"A={area:.4f}")
---
+++ soma_simples.py
@@ -0,0 +1,3 @@
+n1 = int(input())
+n2 = int(input())
+print("SOMA = {}".format(n1 + n2))
